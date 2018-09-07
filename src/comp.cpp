ed21e2ccd9baa2043e0e09f9c7aa96e5f15a51bf
aclouditation <25907312+AClouDitation@users.noreply.github.com>
2018-09-07 10:45:08 -0400

    updated

--- src/comp.cpp
+++ src/comp.cpp
@@ -1,12 +1,11 @@
 #include <string>
 #include <cstdlib>
+#include <fstream>
 #include "../generated/parser.hpp"
-extern int yylex();
-extern int yyparse();
+
 extern FILE* yyin;
-
 int main(int argc, char** argv){
-
+    
     FILE* fp = fopen(argv[1],"r");
     yyin = fp;
     yyparse();
