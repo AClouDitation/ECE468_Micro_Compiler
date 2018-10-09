04c9204497c13484e612ac76b8ac4b05893ffa06
aclouditation <25907312+AClouDitation@users.noreply.github.com>
2018-10-08 20:23:56 -0400

    translate from A3C to Tiny

--- src/comp.cpp
+++ src/comp.cpp
@@ -34,7 +34,17 @@ std::vector<std::string> SplitString(const std::string& s, const std::string& c)
     return v;
 }
 
-void ir2tiny(std::vector<std::string>& irs){
+// translate $T** to r**,
+// probably remove in the future 
+// and re-implement translate()
+std::string t2r(std::string& t){
+    return "r" + t.substr(2);
+}
+
+std::vector<std::string>& ir2tiny(std::vector<std::string>& irs){
+
+    std::vector<std::string>* tiny = new std::vector<std::string>;
+
     for(auto ir:irs){
         std::vector<std::string> items = SplitString(ir, " ");
         if(items[0] == "LABEL"){
@@ -44,37 +54,185 @@ void ir2tiny(std::vector<std::string>& irs){
             // TODO: fill this in the following steps;
         }
         else if(items[0] == "ADDI"){
+            std::string target_reg = t2r(items[3]); 
+            std::string op1;
+            std::string op2;
 
+            if(items[1][0] == '$') op1 = t2r(items[1]);
+            else op1 = items[1];
+
+            if(items[2][0] == '$') op2 = t2r(items[2]);
+            else op2 = items[2];
+
+            std::string new_tiny_op = "move " + op1 + " " + target_reg;
+            tiny->push_back(new_tiny_op);
+            new_tiny_op = "addi " + op2 + " " + target_reg;
+            tiny->push_back(new_tiny_op);
         }
         else if(items[0] == "SUBI"){
+            std::string target_reg = t2r(items[3]); 
+            std::string op1;
+            std::string op2;
+
+            if(items[1][0] == '$') op1 = t2r(items[1]);
+            else op1 = items[1];
+
+            if(items[2][0] == '$') op2 = t2r(items[2]);
+            else op2 = items[2];
+
+            std::string new_tiny_op = "move " + op1 + " " + target_reg;
+            tiny->push_back(new_tiny_op);
+            new_tiny_op = "subi " + op2 + " " + target_reg;
+            tiny->push_back(new_tiny_op);
         }
         else if(items[0] == "MULI"){
+            std::string target_reg = t2r(items[3]); 
+            std::string op1;
+            std::string op2;
+
+            if(items[1][0] == '$') op1 = t2r(items[1]);
+            else op1 = items[1];
+
+            if(items[2][0] == '$') op2 = t2r(items[2]);
+            else op2 = items[2];
+
+            std::string new_tiny_op = "move " + op1 + " " + target_reg;
+            tiny->push_back(new_tiny_op);
+            new_tiny_op = "muli " + op2 + " " + target_reg;
+            tiny->push_back(new_tiny_op);
         }
         else if(items[0] == "DIVI"){
+            std::string target_reg = t2r(items[3]); 
+            std::string op1;
+            std::string op2;
+
+            if(items[1][0] == '$') op1 = t2r(items[1]);
+            else op1 = items[1];
+
+            if(items[2][0] == '$') op2 = t2r(items[2]);
+            else op2 = items[2];
+
+            std::string new_tiny_op = "move " + op1 + " " + target_reg;
+            tiny->push_back(new_tiny_op);
+            new_tiny_op = "divi " + op2 + " " + target_reg;
+            tiny->push_back(new_tiny_op);
         }
         else if(items[0] == "ADDF"){
+            std::string target_reg = t2r(items[3]); 
+            std::string op1;
+            std::string op2;
+
+            if(items[1][0] == '$') op1 = t2r(items[1]);
+            else op1 = items[1];
+
+            if(items[2][0] == '$') op2 = t2r(items[2]);
+            else op2 = items[2];
+
+            std::string new_tiny_op = "move " + op1 + " " + target_reg;
+            tiny->push_back(new_tiny_op);
+            new_tiny_op = "addf " + op2 + " " + target_reg;
+            tiny->push_back(new_tiny_op);
         }
         else if(items[0] == "SUBF"){
+            std::string target_reg = t2r(items[3]); 
+            std::string op1;
+            std::string op2;
+
+            if(items[1][0] == '$') op1 = t2r(items[1]);
+            else op1 = items[1];
+
+            if(items[2][0] == '$') op2 = t2r(items[2]);
+            else op2 = items[2];
+
+            std::string new_tiny_op = "move " + op1 + " " + target_reg;
+            tiny->push_back(new_tiny_op);
+            new_tiny_op = "subf " + op2 + " " + target_reg;
+            tiny->push_back(new_tiny_op);
         }
         else if(items[0] == "MULF"){
+            std::string target_reg = t2r(items[3]); 
+            std::string op1;
+            std::string op2;
+
+            if(items[1][0] == '$') op1 = t2r(items[1]);
+            else op1 = items[1];
+
+            if(items[2][0] == '$') op2 = t2r(items[2]);
+            else op2 = items[2];
+
+            std::string new_tiny_op = "move " + op1 + " " + target_reg;
+            tiny->push_back(new_tiny_op);
+            new_tiny_op = "mulf " + op2 + " " + target_reg;
+            tiny->push_back(new_tiny_op);
         }
         else if(items[0] == "DIVF"){
+            std::string target_reg = t2r(items[3]); 
+            std::string op1;
+            std::string op2;
+
+            if(items[1][0] == '$') op1 = t2r(items[1]);
+            else op1 = items[1];
+
+            if(items[2][0] == '$') op2 = t2r(items[2]);
+            else op2 = items[2];
+
+            std::string new_tiny_op = "move " + op1 + " " + target_reg;
+            tiny->push_back(new_tiny_op);
+            new_tiny_op = "divf " + op2 + " " + target_reg;
+            tiny->push_back(new_tiny_op);
         }
         else if(items[0] == "STOREI"){
+            std::string target_reg; 
+            std::string op1;
+
+            if(items[2][0] == '$') target_reg = t2r(items[2]);
+            else target_reg = items[2];
+
+            if(items[1][0] == '$') op1 = t2r(items[1]);
+            else op1 = items[1];
+
+            tiny->push_back("move " + op1 + " " + target_reg);
         }
         else if(items[0] == "STOREF"){
+            std::string target_reg; 
+            std::string op1;
+
+            if(items[2][0] == '$') target_reg = t2r(items[2]);
+            else target_reg = items[2];
+
+            if(items[1][0] == '$') op1 = t2r(items[1]);
+            else op1 = items[1];
+
+            tiny->push_back("move " + op1 + " " + target_reg);
         }
         else if(items[0] == "READI"){
+            std::string new_tiny_op = "sys readi ";
+            new_tiny_op += items[1];
+            tiny->push_back(new_tiny_op);
         }
         else if(items[0] == "READF"){
+            std::string new_tiny_op = "sys readf ";
+            new_tiny_op += items[1];
+            tiny->push_back(new_tiny_op);
         }
         else if(items[0] == "WRITEI"){
+            std::string new_tiny_op = "sys writei ";
+            new_tiny_op += items[1];
+            tiny->push_back(new_tiny_op);
         }
         else if(items[0] == "WRITEF"){
+            std::string new_tiny_op = "sys writef ";
+            new_tiny_op += items[1];
+            tiny->push_back(new_tiny_op);
         }
         else if(items[0] == "WRITES"){
+            std::string new_tiny_op = "sys writes ";
+            new_tiny_op += items[1];
+            tiny->push_back(new_tiny_op);
         }
     }
+
+    return *tiny;
 }
 
 int main(int argc, char** argv){
@@ -93,15 +251,20 @@ int main(int argc, char** argv){
     for(auto table:symtable_list) delete table;
     
 
+    std::vector<std::string> ops;
     for(auto func_node: func_list){
         std::vector<std::string>& ir = func_node->translate();
-        ir2tiny(ir);
+        std::vector<std::string>& op_block = ir2tiny(ir);
+        ops.insert(ops.end(),op_block.begin(),op_block.end());
         for(auto line: ir){
             std::cout << line << std::endl;
         }
         std::cout << std::endl;
     }
-    //std::vector<std::string>& ir = AST2IR(); 
+
+    for(auto op:ops){
+        std::cout << op << std::endl;
+    }
 
 
 
